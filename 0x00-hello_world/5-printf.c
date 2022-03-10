5c9adfcf6c0eca744e30ea39dcb8ab05a0b138ed
Duchez <edukoo@gmail.com>
2022-03-11 00:18:57 +0300

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry Point
+*
+* Return: 0 if successfull
+*/
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
