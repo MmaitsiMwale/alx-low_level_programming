41645cbb7bb04525559dfb5b6bd32689ca06151e
Duchez <edukoo@gmail.com>
2022-03-15 14:41:44 +0300

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: void
+ *
+ */
+
+void print_alphabet(void)
+{
+	char start;
+	char end = 'z';
+	int mul = 10;
+
+	for (int iter = 1; iter <= mul; iter++)
+	{
+		for (start = 'a'; start <= end; start++)
+		{
+			_putchar(start);
+		}
+	}
+	_putchar('\n');
+}
