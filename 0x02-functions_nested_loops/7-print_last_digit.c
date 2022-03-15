a0cb799d1f95fefeccb3fe9df547fc4ef9d4ff4e
Duchez <edukoo@gmail.com>
2022-03-16 00:32:58 +0300

    print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,47 @@
 #include "main.h"
 /**
+ * check_sign - Prints the sign of a number
+ *
+ * @n: number to be checked
+ *
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than 0
+ */
+
+int check_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+	return (0);
+}
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @n: integer to be computed
+ *
+ * Return: 0 if successful
+ */
+
+int _abs(int n)
+{
+	if (check_sign(n) == 1)
+		return (n);
+	else if (check_sign(n) == -1)
+		return (n * -1);
+	return (0);
+}
+
+/**
  * print_last_digit -prints the last digit of a number
  *
  * @n: number
@@ -11,15 +53,8 @@ int print_last_digit(int n)
 {
 	int mod;
 
-	mod = n % 10;
-	if (mod < 0)
-	{
-		_putchar(-mod + 48);
-		return (-1);
-	}
-	else
-	{
-		_putchar(mod + 48);
-		return (1);
-	}
+	mod = _abs(n % 10);
+
+	_putchar(mod + '0');
+	return (1);
 }
