f692cf93054f24b9680e303049b532d1283f8ccc
Duchez <edukoo@gmail.com>
2022-03-24 17:35:28 +0300

    copy a string for n number of bytes

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "_strlen.c"
+
+/**
+ * _strncpy - copies a string from source (src) to destination (dest)
+ * @src: source of string
+ * @dest: destination of copied string
+ * @n: number of bytes to be copied from src
+ *
+ * Return: pointer to copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int iter;
+
+	for (iter = 0; iter < n; iter++)
+		dest[iter] = src[iter];
+	dest[iter] = '\0';
+	return (dest);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+int _strlen(char *s);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
