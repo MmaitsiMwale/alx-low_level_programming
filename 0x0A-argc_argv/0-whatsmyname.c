fb6e24348ff10d7478b9021f56f6d44e7c578422
Duchez <edukoo@gmail.com>
2022-04-01 13:22:01 +0300

    start

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the program name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if successful
+ */
+
+int main(int __attribute__((unused))argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+<h1>0x0A-argc_argv</h1>
+<h2>This Repo Dives Into Handling CommandLine Arguments</h2>
