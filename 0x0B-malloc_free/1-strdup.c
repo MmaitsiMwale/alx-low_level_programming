826b7eda17d6beb55364defa94f5df65009a0d27
Duchez <edukoo@gmail.com>
2022-04-06 00:29:42 +0300

    duplicates a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,24 +11,23 @@ char *_strdup(char *str)
 {
 	char *dest;
 	unsigned int iter, iter2;
-	
+
 	if (str == NULL)
 		return (NULL);
-	
+
 	iter = 0;
 	while (str[iter])
 		iter++;
-
 	dest = (char *)malloc(sizeof(char) * iter);
-	
+
 	if (dest == NULL)
 		return (NULL);
-	
+
 	iter2 = 0;
 	while (str[iter2])
 	{
 		dest[iter2] = str[iter2];
 		iter2++;
 	}
-	return(dest);
+	return (dest);
 }
