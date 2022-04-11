9f4c32fda58d8f3578f368cad8c89eacd0618fd9
Duchez <edukoo@gmail.com>
2022-04-11 17:55:36 +0300

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,48 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string to be checked
+ *
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s)
+	{
+		s++;
+		count++;
+	}
+	return (count);
+	/*
+	 * char *ptr = s;
+	 * while (*s)
+	 * s++;
+	 * return (s - ptr);
+	 **/
+}
+
+/**
+ * *_strcpy - copies a string including the terminating null byte
+ * @dest: copied string
+ * @src: string to be copied
+ * Return: pointer to new copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	*dest = 0;
+	return (ptr);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of new dog
  * @age: age of new dog
@@ -25,3 +67,4 @@ dog_t *new_dog(char *name, float age, char *owner)
 	another_dog->owner = _strcpy(copy_owner, owner);
 	return (another_dog);
 }
+
