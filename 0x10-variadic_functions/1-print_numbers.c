152ee3f729d2ae9701e1ed0994d0c21e84b5e064
Duchez <edukoo@gmail.com>
2022-04-21 17:28:03 +0300

    print numbers and new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,21 +12,15 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	int x;
-	unsigned int i;
+
+	register unsigned int i;
 	va_list args;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(args, int);
-		if (i == (n - 1))
-		{
-			printf("%d", x);
-			break;
-		}
-		if (!separator)
-			printf("%d%s", x, "");
-		printf("%d%s", x, separator);
+		printf("%d%s", x, (separator && i != n - 1) ? separator : "");
 	}
 	va_end(args);
 	_putchar('\n');
