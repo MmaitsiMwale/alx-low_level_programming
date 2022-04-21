846f094afe9eb087d20e22424d8df72100801749
Duchez <edukoo@gmail.com>
2022-04-21 15:44:11 +0300

    headers

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,7 @@
 #define _VARIADIC_FUNCTIONS_H_
 
 int _putchar(char c);
-int _strlen.c(char *s);
+int _strlen(char *s);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
